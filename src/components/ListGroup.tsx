7cd70fb890b69e049d4d1f9a88e4f5527882abc3
Zeeshan Riasat <zeeshan.riasat302@gmail.com>
2025-03-10 11:04:02 +0500

    conditional rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,24 @@
 function ListGroup() {
-    return (<> <h1>List</h1><ul className="list-group">
-    <li className="list-group-item">An item</li>
-    <li className="list-group-item">A second item</li>
-    <li className="list-group-item">A third item</li>
-    <li className="list-group-item">A fourth item</li>
-    <li className="list-group-item">And a fifth one</li>
-  </ul></>)
+  let list = ["New York", "Tokyo", "San Fransisco", "London"];
+
+  return (
+    <>
+      {" "}
+      <h1>List</h1>
+      {list.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {list.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item, index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
