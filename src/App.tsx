30247e63d6d27bd97de6cac7c10fba4578bb758a
Zeeshan Riasat <zeeshan.riasat302@gmail.com>
2025-03-10 12:46:16 +0500

    passing childrens

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import { Children } from "react";
 function App() {
   let items = ["New York", "Tokyo", "San Fransisco", "London"];
   let handleSelectItem = (item: string) => {
     console.log(item);
   };
+  let children = "Hello World";
   return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
-    </div>
+    // <div>
+    //   <ListGroup
+    //     items={items}
+    //     heading="Cities"
+    //     onSelectItem={handleSelectItem}
+    //   />
+    // </div>
+    <>
+      <Alert children={children} />
+    </>
   );
 }
 
